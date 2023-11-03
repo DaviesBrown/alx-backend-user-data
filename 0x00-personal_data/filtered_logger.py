955e12f4c5f46339143531e4c766045021e426a7
Davies Brown <nwosudavid77@gmail.com>
2023-11-03 03:58:56 +0100

    get db

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,11 +9,6 @@ import mysql.connector
 
 PII_FIELDS: Tuple = ("name", "email", "phone", "ssn", "password")
 
-USERNAME: str = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
-PASSWORD: str = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
-HOST: str = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
-DATABASE: str = os.getenv("PERSONAL_DATA_DB_NAME")
-
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
@@ -68,6 +63,11 @@ def get_logger() -> logging.Logger:
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """ returns a secured connection"""
+    USERNAME: str = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    PASSWORD: str = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    HOST: str = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    DATABASE: str = os.getenv("PERSONAL_DATA_DB_NAME")
+
     connection = mysql.connector.connect(host=HOST,
                                          user=USERNAME,
                                          password=PASSWORD,
